00b00b34cae2aa8e93d8239d91e0abc6fa6d4d47
Rishard Davis <rrdavis777@gmail.com>
2022-02-22 22:41:04 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+ const begin = 42;
+ if (someValue > begin) {
+     return someValue - begin;}
+ else {
+     return begin - someValue;}
+}
+
+function distanceFromHqInFeet(someValue){
+    distanceFromHqInBlocks(someValue);
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+if (start > destination){
+    return (start - destination) * 264;}
+else {
+    return (destination - start) * 264;}
+}
+function calculatesFarePrice(start, destination){
+    distanceTravelledInFeet(start, destination);
+if (distanceTravelledInFeet(start, destination) < 400){
+return 0;
+}
+else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000){
+    return (distanceTravelledInFeet(start, destination) - 400) * 0.02;}
+else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+    return 25;
+}
+else if (distanceTravelledInFeet(start, destination) > 2500) {
+    return 'cannot travel that far'
+}
+}
